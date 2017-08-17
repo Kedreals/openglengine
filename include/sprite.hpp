33fe3c146d702b4cd51b07f4ed7dc99cd9300521
Kedreals <ebert.mueden1@freenet.de>
2017-08-17 23:52:55 +0200

    added sprite.hpp

---
+++ include/sprite.hpp
@@ -0,0 +1,17 @@
+#ifndef _SPRITE_HPP_
+#define _SPRITE_HPP_
+
+/** \todo implement
+    \brief a simple class for drawing 2D pictures
+
+    it is useful to have the abbility to simply draw
+    2D pictures without(with only minimal) depth information.
+    Because of something like a HUD or if you just want a toonlike,
+    sequence.
+ */
+class Sprite
+{
+  
+}
+
+#endif //_SPRITE_HPP_
