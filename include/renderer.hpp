78e20e9a61478c3cfbefdb0b6c557953bcb0f6c4
Kedreals <ebert.mueden1@freenet.de>
2017-08-25 13:00:46 +0200

    started trying to implement a renderer but first have to check something

--- include/renderer.hpp
+++ include/renderer.hpp
@@ -1,10 +1,26 @@
 #ifndef _RENDERER_HPP_
 #define _RENDERER_HPP_
 
+class Game;
+class Model;
+
 /** \todo implement
  */
 class Renderer
 {
+public:
+  Renderer();
+  ~Renderer();
+
+  void Initialize();
+  void Draw(const Model& model);
+
+protected:
+  void DrawToScreen() const;
+  friend class Game;
+
+private:
+std::vector<glm::vec3>
 };
 
 #endif //_RENDERER_HPP_
